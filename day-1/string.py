d57784df041082700aef754e0a738518370bde30
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-05-16 15:51:01 +0530

    Day 1 string and debugging

---
+++ day-1/string.py
@@ -0,0 +1,15 @@
+# string concatenation
+print("Hello" + " " + "World!")
+
+# Debugging
+
+# print(day 1 - string manipulation")
+# print("String concatenation is done with the " + " sign.")
+    # print('e.g. print("Hello" + "World")')
+# print(("New lines can be created with a backslash and n.")
+
+# Solution
+print("Day 1 - String Manipulation")
+print('string concatenation is done with the "+" sign.')
+print('e.g. print("Hello" + "World")')
+print("New lines can be created with a backslash and n.")
