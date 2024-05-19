5afc3638e9a2b0d75a0d57361c8f4b8f240311ed
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-05-19 17:08:39 +0530

    Day one project

---
+++ day-1/day-one.project.py
@@ -0,0 +1,10 @@
+print("Wlcome to the brand name generator:")
+
+# Taking city as input
+city = input("What's the name of city you grew up in? ")
+
+# Taking pet as input
+pet = input("What's your pet's name? ")
+
+# printing the band name
+print('Your band name could be ' + city + " " + pet)
--- day-1/input-function.py
+++ day-1/input-function.py
@@ -1,9 +1,9 @@
 # store the input in a variable and print it.
-# x = input("what is you name?\n")
-# print("Your name is " + x + "\n")
+x = input("what is you name?\n")
+print("Your name is " + x + "\n")
 
 # function inside a function
-# print("Hello " + input("What is your name? "))
+print("Hello " + input("What is your name? "))
 
 # input name fron user and print it's length
 name = input("What is your name? ")
