1b90ebac4b32abfbdb0148f9121eb625caeb3f12
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-05-19 13:21:44 +0530

    Input function

---
+++ day-1/input-function.py
@@ -0,0 +1,10 @@
+# store the input in a variable and print it.
+# x = input("what is you name?\n")
+# print("Your name is " + x + "\n")
+
+# function inside a function
+# print("Hello " + input("What is your name? "))
+
+# input name fron user and print it's length
+name = input("What is your name? ")
+print(len(name))
