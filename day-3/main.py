2eb271d31a513a6047c955d941008f6adb8019b6
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-05-29 00:23:01 +0530

    Day 3 if else

---
+++ day-3/main.py
@@ -0,0 +1,9 @@
+# if-else
+
+# WAP to check if a number is odd or even
+number = int(input("Enter any number: "))
+
+if number % 2 == 0:
+    print(number, "is a even number.")
+else:
+    print(number, "is a odd number.")
