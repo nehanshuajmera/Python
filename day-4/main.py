f8cc072ececce8e41bc12857f331c10f0ad66c91
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-05-31 17:34:32 +0530

    Random heads and tails program

--- day-4/main.py
+++ day-4/main.py
@@ -3,10 +3,19 @@
 import random
 
 # Random Integer
-integer = random.randint(1, 10)
-print(integer)
+# integer = random.randint(1, 10)
+# print(integer)
 
 # Random Float
-floating = random.random()
-print(floating)
+# floating = random.random()
+# print(floating)
 
+# ------------ X ------------
+
+# WAP to print head and tails (randomily)
+toss = random.randint(1, 2)
+
+if toss == 1:
+    print("Heads")
+else:
+    print("Tails")
