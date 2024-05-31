55677bccf9bb8080014db32931c5e3ef67dfef46
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-05-31 19:49:32 +0530

    intro with Lists

--- day-4/main.py
+++ day-4/main.py
@@ -13,9 +13,21 @@ import random
 # ------------ X ------------
 
 # WAP to print head and tails (randomily)
-toss = random.randint(1, 2)
+# toss = random.randint(1, 2)
 
-if toss == 1:
-    print("Heads")
-else:
-    print("Tails")
+# if toss == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# ------------ X ------------
+
+# Lists
+
+names = input("Enter everyone's name seperated by comma.\n")
+names_list = names.split(", ")
+print(names_list)
+print(len(names_list))
+random_card = random.randint(0, len(names_list)-1)
+print(random_card)
+print(names_list[random_card])
