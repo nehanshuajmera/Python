8560ed449b46a9cbe0644938c2cdfd92557f3cc7
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-06-01 15:36:32 +0530

    Printing ther average height of students

--- day-5/main.py
+++ day-5/main.py
@@ -1,7 +1,22 @@
 # loops
 
 # 1. For loop
-fruits = ["apple", "mango", "orange"]
+# fruits = ["apple", "mango", "orange"]
 
-for fruit in fruits:
-    print(fruit)
+# for fruit in fruits:
+#     print(fruit)
+
+# ------------ X ---------------
+
+# WAP to print average height of students
+
+height_list = input("Enter the height of students.\n").split(" ")
+print(height_list)
+length = len(height_list)
+sum = 0
+
+for height in height_list:
+    sum += int(height)
+
+average = round(sum / length)
+print(average)
