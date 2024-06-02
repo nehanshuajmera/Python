b2be2f4a18ac7df172da56f5bd5599b7004d5591
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-06-02 10:24:46 +0530

    Hangman done

--- day-7/main.py
+++ day-7/main.py
@@ -10,29 +10,28 @@ word = []
 for blank in chosen_word:
     word += "_"
 
-print(word)
-
 lives = 6
 end = False
 
 while not end:
-    guess = input("Guess any letter. ").lower()
+    guess = input("\nGuess any letter. ").lower()
 
     for i in range(0, len(chosen_word)):
         if lives != 0 and chosen_word[i] == guess:
             word[i] = guess
-            
-    print(word)
+
+    # print(word)
+    print("\n",' '.join(word))
         
     if guess not in chosen_word:
         lives -= 1
         if lives == 0:
             end = True
-            print("You Lose!!!!!")
+            print("\nYou Lose!!!!!")
 
-    print("You have", lives, "lives left")
+    print("\nYou have", lives, "lives left")
 
     if "_" not in word:
         end = True
-        print("You Win!!!!!")
+        print("\nYou Win!!!!!")
         
