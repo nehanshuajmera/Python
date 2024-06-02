385699d3680078a1bb163b65622168b0d9c0f682
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-06-02 07:49:41 +0530

    While loop

--- day-6/main.py
+++ day-6/main.py
@@ -1,8 +1,16 @@
 # Function
 
 # defining the function
-def my_fun():
-    print("Hello")
+# def my_fun():
+#     print("Hello")
 
 # calling the function
-my_fun()
+# my_fun()
+
+# while loop
+
+number = 6
+while number > 0:
+    print("Hello")
+    number -= 1
+
