4895d3890dc1f36a382629f91f07183930ffdb5b
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-06-02 08:57:11 +0530

    Hangman lvl 2 done

--- day-7/main.py
+++ day-7/main.py
@@ -5,7 +5,7 @@ import random
 word_list = ["advark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-
+print(chosen_word)
 word = []
 for blank in chosen_word:
     word += "_"
@@ -14,8 +14,11 @@ print(word)
 
 guess = input("Guess any letter. ").lower()
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
+for i in range(0, len(chosen_word)):
+    if chosen_word[i] == guess:
+        print(guess)
+        word[i] = guess
     else:
         print("Wrong")
+
+print(word)
