41f45cdfa5f736c615f742518a6df7874505f064
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-06-03 23:39:07 +0530

    More in functions

--- day-7/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-guess = input("Guess any letter. ").lower()
-
-# for letter in chosen_word:
-#     if letter == guess:
-#         print("Right")
-#     else:
-#         print("Wrong")
---
+++ day-8/main.py
@@ -0,0 +1,8 @@
+# More on functions
+
+def greet():
+    print("Hello,")
+    print("How are you??")
+    print("What are you doing??")
+
+greet()
