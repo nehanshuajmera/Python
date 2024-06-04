b7715308e266edfe2143445f78515a78ce454439
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-06-04 19:59:42 +0530

    Day 8 program practice 1

--- day-8/main.py
+++ day-8/main.py
@@ -1,3 +1,4 @@
+import math
 # More on functions
 # def greet():
 #     print("Hello,")
@@ -28,8 +29,21 @@
 # greet("Indore", "Nehanshu") # Positional arguments
 
 # KeyWord Arguments
-def practice(a, b, c):
-    print("A is",a, "\nB is",b, "\nC is",c)
+# def practice(a, b, c):
+#     print("A is",a, "\nB is",b, "\nC is",c)
 
-practice(1, 2, 3)
-practice(c = 3, a = 1, b = 2) # keyword arguments
+# practice(1, 2, 3)
+# practice(c = 3, a = 1, b = 2) # keyword arguments
+
+# WAP to get no. of can needed to paint a wall
+
+def cans_needed(height, width, cover):
+    area = height * width 
+    no_of_cans = math.ceil(area / cover)
+    print(no_of_cans)
+
+height = int(input("Height of wall. "))
+width = int(input("Width of wall. "))
+coverage = 5
+
+cans_needed(height=height, width=width, cover=coverage)
