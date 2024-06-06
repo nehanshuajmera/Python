e555132cc8f8cf799263f3e4935eb25026ba5767
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-06-06 23:29:13 +0530

    Day 8

--- day-8/main.py
+++ day-8/main.py
@@ -1,54 +1,53 @@
 import math
 # More on functions
-# def greet():
-#     print("Hello,")
-#     print("How are you??")
-#     print("What are you doing??")
+def greet():
+    print("Hello,")
+    print("How are you??")
+    print("What are you doing??")
 
-# greet()
+greet()
 
 # Function with Input
-# def greet(name):
-#     print("Hello", name)
+def greet(name):
+    print("Hello", name)
 
-# greet("Nehanshu")
+greet("Nehanshu")
 
 # Functions with multiple input
-# def greet(name, location):
-#     print("Name is", name)
-#     print("Lives in", location)
+def greet(name, location):
+    print("Name is", name)
+    print("Lives in", location)
 
-# greet("Nehanshu", "Indore")
+greet("Nehanshu", "Indore")
 
 # positional arguments
-# def greet(name, location):
-#     print("Name is", name)
-#     print("Lives in", location)
+def greet(name, location):
+    print("Name is", name)
+    print("Lives in", location)
 
-# greet("Nehanshu", "Indore")
-# greet("Indore", "Nehanshu") # Positional arguments
+greet("Nehanshu", "Indore")
+greet("Indore", "Nehanshu") # Positional arguments
 
 # KeyWord Arguments
-# def practice(a, b, c):
-#     print("A is",a, "\nB is",b, "\nC is",c)
+def practice(a, b, c):
+    print("A is",a, "\nB is",b, "\nC is",c)
 
-# practice(1, 2, 3)
-# practice(c = 3, a = 1, b = 2) # keyword arguments
+practice(1, 2, 3)
+practice(c = 3, a = 1, b = 2) # keyword arguments
 
 # WAP to get no. of can needed to paint a wall
-# def cans_needed(height, width, cover):
-#     area = height * width 
-#     no_of_cans = math.ceil(area / cover)
-#     print(no_of_cans)
+def cans_needed(height, width, cover):
+    area = height * width 
+    no_of_cans = math.ceil(area / cover)
+    print(no_of_cans)
 
-# height = int(input("Height of wall. "))
-# width = int(input("Width of wall. "))
-# coverage = 5
+height = int(input("Height of wall. "))
+width = int(input("Width of wall. "))
+coverage = 5
 
-# cans_needed(height=height, width=width, cover=coverage)
+cans_needed(height=height, width=width, cover=coverage)
 
 # WAP to know the prime number
-
 def get_prime(number):
     for i in range(2, round(number / 2)):
         if number % i == 0:
