d03fae95805fdfc6a7d08f318c47ebade22225d6
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-06-09 13:05:06 +0530

    Day 8 project part 2 - decypher text

--- day-8/project.py
+++ day-8/project.py
@@ -1,6 +1,6 @@
 # WAP to encrypt and decrypt the message
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def encode(message, shift):
     cypher_text = ""
@@ -11,9 +11,22 @@ def encode(message, shift):
         cypher_text += new_letter
     print("\nEncrypted message is:",cypher_text)
 
+def decode(message, shift):
+    decypher_text = ""
+    for letter in message:
+        pos = alphabet.index(letter)
+        new_pos = pos - shift
+        new_letter = alphabet[new_pos]
+        decypher_text += new_letter
+    print("\nDecrypted message is:", decypher_text)
+
 dir = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 message = input("\nType yor message:\n").lower()
 shift = int(input("\nType the shift number:\n"))
 
 if dir == "encode":
     encode(message = message, shift = shift)
+elif dir == "decode":
+    decode(message = message, shift = shift)
+else:
+    print("Type the correct input")
