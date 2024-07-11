a6d47b2b0bf75eae1c5970e0ed1d51b0f813ca13
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-07-11 19:05:16 +0530

    nesting list inside dictionary

--- day-9/dictionary.py
+++ day-9/dictionary.py
@@ -1,40 +1,50 @@
 # Dictionaries in python
 
 # Creating a dictionary
-dict = {
-    "one" : "value1",
-    "two" : "value2",
-    "three" : "value3",
-    "four" : "value4",
-}
+# dict = {
+#     "one" : "value1",
+#     "two" : "value2",
+#     "three" : "value3",
+#     "four" : "value4",
+# }
 
 # Accessing values
-print(dict["one"])
-print(dict["two"])
-print(dict["three"])
-print(dict["four"])
+# print(dict["one"])
+# print(dict["two"])
+# print(dict["three"])
+# print(dict["four"])
 
 # Printing dictionary
-print(dict)
+# print(dict)
 
 # Adding new key value pair
-dict["five"] = "value5"
+# dict["five"] = "value5"
 
 # Printing dictionary
-print(dict)
+# print(dict)
 
 # Removing key value pair
-del dict["two"]
+# del dict["two"]
 
 # Printing dictionary
-print(dict)
+# print(dict)
 
 # Checking if key exists
-print("one" in dict)
+# print("one" in dict)
 
 # loops in dictionary
-for key in dict:
-    print(key, dict[key])
+# for key in dict:
+#     print(key, dict[key])
 
-for value in dict.values():
-    print(value)
+# for value in dict.values():
+#     print(value)
+
+# nesting in dictionary
+
+# 1. nesting list in dictionary
+travel_log = {
+    "france" : ["paris", "lyon"],
+    "germany" : ["berlin", "munich"],
+}
+
+print(travel_log)
