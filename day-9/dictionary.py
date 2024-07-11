327654b50f51638bd84673e99f898956634cb308
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-07-11 19:29:40 +0530

    nested dictionary in list

--- day-9/dictionary.py
+++ day-9/dictionary.py
@@ -42,9 +42,46 @@
 # nesting in dictionary
 
 # 1. nesting list in dictionary
-travel_log = {
-    "france" : ["paris", "lyon"],
-    "germany" : ["berlin", "munich"],
-}
+# travel_log = {
+#     "france" : ["paris", "lyon"],
+#     "germany" : ["berlin", "munich"],
+# }
+
+# print(travel_log)
+
+# 2. nesting dictionary in dictionary
+# travel_log = {
+#     "france" : {
+#         "cities_visited" : ["paris", "lyon"],
+#         "total_visits" : 3,
+#     },
+#     "germany" : {
+#         "cities_visited" : ["berlin", "munich"],
+#         "total_visits" : 12,
+#     }
+# }
+
+# print(travel_log)
+
+# for country in travel_log:
+#     print(country)
+#     print(travel_log[country])
+
+# 3. nesting dictionary in list
+travel_log = [
+    {
+        "country" : "India",
+        "total_visits" : 12,
+        "cities_visited" : ["Delhi", "Mumbai", "Indore"],
+    },
+    {
+        "country" : "Korea",
+        "total_visits" : 2,
+        "cities_visited" : ["Seoul", "Busan"],
+    },
+]
 
 print(travel_log)
+
+for country in travel_log:
+    print(country)
