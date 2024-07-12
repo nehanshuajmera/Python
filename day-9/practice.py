eb0507a885aa25d169294f26ba116036165f5483
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-07-12 17:55:48 +0530

    Practice old lessons

--- day-9/practice.py
+++ day-9/practice.py
@@ -1,37 +1,74 @@
 # Practice 
 
 # round function round off the decimal value to the nearest integer
-print( round(8 / 3) )
-print( round(7 / 3) )
+# print( round(8 / 3) )
+# print( round(7 / 3) )
 
 # backslash is used to treat anything just after it as string
-print("you're doing well, welcome to the 1 million club, \"Whoooa\"")
+# print("you're doing well, welcome to the 1 million club, \"Whoooa\"")
 
 # function with parameter
-def func(name):
-    print("hello", name)
+# def func(name):
+#     print("hello", name)
 
-func("Nehanshu")
+# func("Nehanshu")
 
 # function without parameter
-def new_func():
-    print("Function Without Parameter is Called")
+# def new_func():
+#     print("Function Without Parameter is Called")
 
-new_func()
+# new_func()
 
 # lists
-fruits = ["apple", "mango", "banana"]
+# fruits = ["apple", "mango", "banana"]
 
-for fruit in fruits:
-    print(fruit)
+# for fruit in fruits:
+#     print(fruit)
 
-for index in range(len(fruits)):
-    print(fruits[index])
+# for index in range(len(fruits)):
+#     print(fruits[index])
 
-print( fruits[0] )
-print( fruits[1] )
-print( fruits[2] )
-print( fruits[-1] )
+# print( fruits[0] )
+# print( fruits[1] )
+# print( fruits[2] )
+# print( fruits[-1] )
 
 # index function to get index
-print( fruits.index("banana"))
+# print( fruits.index("banana"))
+
+# 12/07/2024
+
+# print(12) # integer
+# print("Nehanshu") #string
+# print(3.556) #float
+# print(True) #boolean
+
+# name = input("What is your name? ")
+# print("Your name is ",name)
+
+# input(prompt)
+
+# num = int(input("enter any number? "))
+# print(num)
+# print(type(num))
+# print(5 + num)
+
+# name = "Nehanshu"
+# print(len(name))
+
+# print( "Nehanshu " + "Ajmera")
+# print("Nehanshu \nAjmera")
+
+# print(20 + 20) #addition
+# print(40 - 23) # substraction
+# print(4 * 2) # multiplication
+# print(20 / 2) # division
+# print(2 ** 3) # power
+ 
+# if else 
+number = int(input("Enter your age? "))
+
+if number > 18:
+    print("You are eligible to vote")
+else:
+    print("You are a minor, not eligible to vote")
