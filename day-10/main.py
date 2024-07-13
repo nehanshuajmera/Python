c0505dfccc234978680333ee070c939105f7bf86
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-07-13 18:09:36 +0530

    DocStrings

--- day-10/main.py
+++ day-10/main.py
@@ -1,9 +1,10 @@
-# WAP to conver name to title case
+# WAP to convert name to title case
 
 f_name = input("Enter your first name: ")
 l_name = input("Enter your last name: ")
 
 def title_case(first, last):
+    """ This is function used to change strings to Title case """ # DocString (used for Documentation purpose.)
     full_name = first + " " + last
     title_name = full_name.title()
     return title_name
