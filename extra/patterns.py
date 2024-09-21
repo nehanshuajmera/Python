86ef31790fcde2ef186d3945d8ea05420acd7f83
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-09-21 15:03:06 +0530

    Practicing patterns

--- extra/patterns.py
+++ extra/patterns.py
@@ -20,16 +20,35 @@
 # 4 5 6
 # 7 8 9
 
+# n = int(input())
+
+# count = 1
+# i = 1
+
+# while (i <= n):
+#   j = 1
+#   while (j <= n):
+#     print(count, end=" ")
+#     count = count + 1
+#     j = j + 1
+#   print()
+#   i = i + 1
+
+#  --------------------------- X --------------- 
+
+# Wap ro print
+# *
+# * *
+# * * *
+# * * * *
+
 n = int(input())
 
-count = 1
 i = 1
-
-while (i <= n):
+while(i <= n):
   j = 1
-  while (j <= n):
-    print(count, end=" ")
-    count = count + 1
+  while(j <= i):
+    print("*", end=" ")
     j = j + 1
   print()
   i = i + 1
