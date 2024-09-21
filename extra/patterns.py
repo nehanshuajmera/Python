9e333db72fafd20ff31826d63229692b9b333d78
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-09-21 15:13:07 +0530

    Working with patterns

--- extra/patterns.py
+++ extra/patterns.py
@@ -61,12 +61,31 @@
 # 3 3 3
 # 4 4 4 4
 
+# n = int(input())
+# i = 1
+# while(i <= n):
+#   j = 1
+#   while(j <= i):
+#     print(i, end=" ")
+#     j = j + 1
+#   print()
+#   i = i + 1
+
+#  --------------------------- X --------------- 
+
+#Wap to print
+# 1
+# 2 3
+# 4 5 6
+
 n = int(input())
+count = 1
 i = 1
 while(i <= n):
   j = 1
   while(j <= i):
-    print(i, end=" ")
+    print(count, end=" ")
+    count = count + 1
     j = j + 1
   print()
   i = i + 1
