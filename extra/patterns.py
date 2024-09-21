9dbb0b7d91806e439b8905fa74474f17647a3b49
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-09-21 14:58:00 +0530

    Printing patters

--- extra/patterns.py
+++ extra/patterns.py
@@ -3,12 +3,33 @@
 # 2 2 2 
 # 3 3 3 
 
+# n = int(input())
+# i = 1
+# while (i <= n):
+#   j = 1
+#   while (j <= n):
+#     print(i, end=" ")
+#     j = j + 1
+#   print("\n")
+#   i = i + 1
+
+#  --------------------------- X --------------- 
+
+# Wap to print
+# 1 2 3
+# 4 5 6
+# 7 8 9
+
 n = int(input())
+
+count = 1
 i = 1
+
 while (i <= n):
   j = 1
   while (j <= n):
-    print(i, end=" ")
+    print(count, end=" ")
+    count = count + 1
     j = j + 1
-  print("\n")
+  print()
   i = i + 1
